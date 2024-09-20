ad629a288093973acdc0d238fa75b4220edf7bf0
Abhijith Acharya <abhijith.acharya@epsilon.com>
2024-09-20 17:42:02 +0530

    header logo added

--- Musiefy-App/src/Components/Header.jsx
+++ Musiefy-App/src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faHouse, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+import NodeImg from "../assets/Node.png"
 
 function Header(){
     return(
         <header className="flex justify-between items-center px-5 py-2">
-            <div><img src="" alt="header-logo" /></div>
+            <div><img src={NodeImg} alt="header-logo" className="max-w-16"/></div>
             <div className="flex items-center gap-3">
                 <button className="bg-zinc-800 w-10 aspect-square rounded-full text-gray-400 hover:text-gray-200"><FontAwesomeIcon icon={faHouse} /></button>
                 <span className="min-w-[300px] md:min-w-[400px] py-2 px-1 rounded-3xl bg-zinc-800 flex justify-center hover:bg-zinc-700 focus-within:outline-white focus-within:outline-2 focus-within:outline relative">
